7e53c73e3b1e7c270455a19c534099b6495abbea
Jonny Cheetham <jcheetham@amplience.com>
2023-10-26 16:18:42 +0100

    fix: CMP-10661 Importing medium weight font and updated font sizes

--- packages/extension/src/index.tsx
+++ packages/extension/src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "@fontsource/ibm-plex-sans";
+import "@fontsource/ibm-plex-sans/500.css";
 import App from "./App";
 import "./index.css";
 
