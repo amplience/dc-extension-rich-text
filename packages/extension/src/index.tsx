46e0527c5012f1935087fa712e1db1bb65a9b655
Chattox <33847899+Chattox@users.noreply.github.com>
2023-10-26 16:53:07 +0100

    feat: CMP-10574 add gainsight custom events for credit use and limit reached

--- packages/extension/src/index.tsx
+++ packages/extension/src/index.tsx
@@ -3,5 +3,8 @@ import ReactDOM from "react-dom";
 import "@fontsource/ibm-plex-sans";
 import App from "./App";
 import "./index.css";
+import { initGainsight } from "./gainsight";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+initGainsight();
