bd21b13b20a38d6d5f9f9bfb16d321a288b2f63c
Andrew Gil <andrew.gil@newelevation.com>
2025-04-02 16:59:56 -0500

    feat: add optional chaining support for Amplience module in Storybook

--- packages/extension/.storybook/webpack.config.js
+++ packages/extension/.storybook/webpack.config.js
@@ -1,17 +1,29 @@
-module.exports = ({
-  config
-}) => {
+module.exports = ({ config }) => {
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
-    use: [{
-        loader: require.resolve('awesome-typescript-loader'),
-      },
-      // Optional
+    use: [
       {
-        loader: require.resolve('react-docgen-typescript-loader'),
+        loader: require.resolve("awesome-typescript-loader"),
+      },
+      {
+        loader: require.resolve("react-docgen-typescript-loader"),
       },
     ],
   });
-  config.resolve.extensions.push('.ts', '.tsx');
+
+  // Add a new rule specifically for the Amplience module
+  config.module.rules.push({
+    test: /\.js$/,
+    include: /node_modules\/@amplience/,
+    use: {
+      loader: require.resolve("babel-loader"),
+      options: {
+        presets: ["@babel/preset-env"],
+        plugins: ["@babel/plugin-proposal-optional-chaining"],
+      },
+    },
+  });
+
+  config.resolve.extensions.push(".ts", ".tsx");
   return config;
 };
